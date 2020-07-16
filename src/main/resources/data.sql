8984d45ecb79d13fde61473c9284dda4e657839e
Christian Surio <ChristianSurio404@gmail.com>
2020-07-16 17:38:54 -0600

    db properties 04

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
-INSERT INTO role (descripcion, nombre) VALUES ('administrador', 'admin');
-INSERT INTO role (descripcion, nombre) VALUES ('alumno', 'estudiante');
-INSERT INTO role (descripcion, nombre) VALUES ('profesor de materia', 'docente');
-INSERT INTO role (descripcion, nombre) VALUES ('jefe de departamento', 'jefe');
-INSERT INTO role (descripcion, nombre) VALUES ('representante de curso', 'representante');
+INSERT INTO role (id,descripcion, nombre) VALUES (1,'administrador', 'admin');
+INSERT INTO role (id,descripcion, nombre) VALUES (2,'alumno', 'estudiante');
+INSERT INTO role (id,descripcion, nombre) VALUES (3,'profesor de materia', 'docente');
+INSERT INTO role (id,descripcion, nombre) VALUES (4,'jefe de departamento', 'jefe');
+INSERT INTO role (id,descripcion, nombre) VALUES (5,'representante de curso', 'representante');
