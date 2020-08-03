cb29d38e9d4a1c4152c725359a6bf742921e6def
Christian Surio <ChristianSurio404@gmail.com>
2020-08-03 16:36:46 -0600

    spring security, proteccion de url´s falta encriptar contraseñas

--- src/main/java/com/sigsaaqyf/demo/WebSecurityConfig.java
+++ src/main/java/com/sigsaaqyf/demo/WebSecurityConfig.java
@@ -1,9 +1,16 @@
 package com.sigsaaqyf.demo;
 
+import com.sigsaaqyf.demo.service.UserDetailsServiceImpl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity // para sobreescribir la config. por defecto
@@ -15,13 +22,37 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http
         .authorizeRequests()
         .antMatchers(resources).permitAll()
-        .antMatchers("/","/index").permitAll()
+        .antMatchers("/","/index","/userRegister").permitAll()
             .anyRequest().authenticated()
             .and()
         .formLogin()
-            .loginPage(loginPage)
+            .loginPage("/login")
+            .permitAll()
+            .defaultSuccessUrl("/userList")
+            .failureUrl("/login?error=true")
+            .usernameParameter("carnet")   //name de los inputs del formulario html
+            .passwordParameter("password") //name de los inputs del formulario html
+            .and()
+            .csrf().disable()
+        .logout()
+            .permitAll()
+            .logoutSuccessUrl("/login?logout");
         //super.configure(http);
     }
 
-    
+    BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        bCryptPasswordEncoder = new BCryptPasswordEncoder(4);
+        return bCryptPasswordEncoder;
+    }
+
+    @Autowired
+    UserDetailsService userDetailsService;
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
 }
